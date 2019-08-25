fb54808ecdd038d979891c752456fae152e688f6
Yura Lim <yura6068@gmail.com>
2019-08-25 08:06:52 +0000

    feat: Add the sample ETL jupyter notebook

---
+++ create_tables.sql
@@ -0,0 +1,81 @@
+DROP TABLE IF EXISTS public.business;
+DROP TABLE IF EXISTS public.business_category;
+DROP TABLE IF EXISTS public.review;
+DROP TABLE IF EXISTS public.review_text;
+DROP TABLE IF EXISTS public.tip;
+DROP TABLE IF EXISTS public.tip_text;
+DROP TABLE IF EXISTS public.checkin;
+DROP TABLE IF EXISTS public.yelp_user;
+DROP TABLE IF EXISTS public.yelp_user_elite;
+DROP TABLE IF EXISTS public.yelp_user_friend;
+
+CREATE TABLE public.business (
+  business_id varchar(32) PRIMARY KEY,
+  "address" varchar(256),
+  city varchar(256),
+  latitude double precision,
+  longitude double precision,
+  "name" varchar(256),
+  postal_code varchar(256),
+  "state" varchar(2)
+);
+
+CREATE TABLE public.business_category (
+  business_id varchar(32) NOT NULL,
+  category varchar(128)
+)
+diststyle key distkey (business_id);
+
+CREATE TABLE public.review (
+  review_id varchar(32) PRIMARY KEY,
+  "user_id" varchar(32) NOT NULL,
+  business_id varchar(32) NOT NULL,
+  stars double precision,
+  "date" timestamp,
+  "text" varchar(max),
+  sentiment varchar(8)
+);
+
+CREATE TABLE public.review_text (
+  review_id varchar(32) NOT NULL,
+  word varchar(256) 
+)
+diststyle key distkey (review_id);
+
+CREATE TABLE public.tip (
+  tip_id varchar(36) NOT NULL,
+  "user_id" varchar(32) NOT NULL,
+  "date" timestamp,
+  business_id varchar(32) NOT NULL,
+  "text" varchar(max),
+  sentiment varchar(8)
+);
+
+CREATE TABLE public.tip_text (
+  tip_id varchar(36) NOT NULL,
+  word varchar(256)
+)
+diststyle key distkey (tip_id);
+
+CREATE TABLE public.checkin (
+  business_id varchar(32) NOT NULL,
+  "date" timestamp
+)
+diststyle key distkey (business_id);
+
+CREATE TABLE public.yelp_user (
+  "user_id" varchar(32) PRIMARY KEY,
+  "name" varchar(256)
+);
+
+CREATE TABLE public.yelp_user_elite (
+  "user_id" varchar(32) NOT NULL,
+  "year" varchar(4) NOT NULL
+)
+diststyle key distkey (user_id);
+
+CREATE TABLE public.yelp_user_friend (
+  "user_id" varchar(32) NOT NULL,
+  "friend_id" varchar(32) NOT NULL
+)
+diststyle key distkey (user_id);
---
+++ redshift/create_cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+aws redshift create-cluster \
+--cluster-identifier redshift-cluster \
+--db-name dev \
+--cluster-type single-node \
+--master-username $REDSHIFT_USERNAME \
+--master-user-password $REDSHIFT_PASSWORD \
+--node-type dc2.large \
+--vpc-security-group-ids $REDSHIFT_SECURITY_GROUP \
+--iam-roles $REDSHIFT_IAM_ROLE
