5f37968a0efd5a514f80b895edc533dde487f81a
ylim <ylim@equilar.com>
2019-09-04 01:15:13 -0700

    fix: Fix wrong column types and Add script_log table for Airflow data pipeline.

--- create_tables.sql
+++ create_tables.sql
@@ -8,6 +8,7 @@ DROP TABLE IF EXISTS public.checkin;
 DROP TABLE IF EXISTS public.yelp_user;
 DROP TABLE IF EXISTS public.yelp_user_elite;
 DROP TABLE IF EXISTS public.yelp_user_friend;
+DROP TABLE IF EXISTS public.script_log;
 
 CREATE TABLE public.business (
   business_id varchar(32) PRIMARY KEY,
@@ -17,7 +18,7 @@ CREATE TABLE public.business (
   longitude double precision,
   "name" varchar(256),
   postal_code varchar(256),
-  "state" varchar(2)
+  "state" varchar(32)
 );
 
 CREATE TABLE public.business_category (
@@ -38,12 +39,12 @@ CREATE TABLE public.review (
 
 CREATE TABLE public.review_text (
   review_id varchar(32) NOT NULL,
-  word varchar(256) 
+  word varchar(max) 
 )
 diststyle key distkey (review_id);
 
 CREATE TABLE public.tip (
-  tip_id varchar(36) NOT NULL,
+  tip_id varchar(36) PRIMARY KEY,
   "user_id" varchar(32) NOT NULL,
   "date" timestamp,
   business_id varchar(32) NOT NULL,
@@ -53,7 +54,7 @@ CREATE TABLE public.tip (
 
 CREATE TABLE public.tip_text (
   tip_id varchar(36) NOT NULL,
-  word varchar(256)
+  word varchar(max)
 )
 diststyle key distkey (tip_id);
 
@@ -79,3 +80,9 @@ CREATE TABLE public.yelp_user_friend (
   "friend_id" varchar(32) NOT NULL
 )
 diststyle key distkey (user_id);
+
+CREATE TABLE public.script_log (
+  script_name varchar(32) NOT NULL,
+  processed_file smallint
+)
+diststyle key distkey (script_name);
